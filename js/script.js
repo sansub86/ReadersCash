457d7b0dff043013da6867effa39bd0d7cb06223
sansub86 <sun_mail_86@mail.ru>
2017-08-18 11:17:03 +1000

    адаптивность

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $(document).height()}, 'slow');
         return false;
     });
+    $(".menu-burger").click(function () {
+        $(".main-menu").toggleClass("main-menu--hide");
+    });
+    $(".close").click(function () {
+       $(".main-menu").toggleClass("main-menu--hide");
+    });
 });
